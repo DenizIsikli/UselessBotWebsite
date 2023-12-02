2e85f5e2e7d6413248b22c46590480d9ad4d9df5
Bobby <s215818@dtu.dk>
2023-12-02 02:30:25 +0100

    Added tooltip

--- src/website/frontpage.tsx
+++ src/website/frontpage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './frontpage.css';
+import { FaGithub } from 'react-icons/fa';
 
 const Frontpage: React.FC = () => {
   return (
@@ -25,98 +26,232 @@ const Frontpage: React.FC = () => {
           <div className="subcommands">
             <h2>General Command List</h2>
             <ul>
-              <li>!help1: Command List 1</li>
-              <li>!help2: Command List 2</li>
-              <li>!help3: Command List 3</li>
-              <li>!help4: Command List 4</li>
-              <li>!help5: Command List 5</li>
-              <li>!help6: Command List 6</li>
-              <li>!commandlist: Command List Overview</li>
+              <li>
+                !help1: Command List 1
+                <span className="tooltip">Description for !help1</span>
+              </li>
+              <li>
+                !help2: Command List 2
+                <span className="tooltip">Description for !help2</span>
+              </li>
+              <li>
+                !help3: Command List 3
+                <span className="tooltip">Description for !help3</span>
+              </li>
+              <li>
+                !help4: Command List 4
+                <span className="tooltip">Description for !help4</span>
+              </li>
+              <li>
+                !help5: Command List 5
+                <span className="tooltip">Description for !help5</span>
+              </li>
+              <li>
+                !help6: Command List 6
+                <span className="tooltip">Description for !help6</span>
+              </li>
+              <li>
+                !commandlist: Command List Overview
+                <span className="tooltip">Description for !commandlist</span>
+              </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 1 (HelpEmbed1)</h2>
             <ul>
-                <li>!urban</li>
-                <li>!qrcode</li>
-                <li>!imgur</li>
-                <li>!wiki</li>
-                <li>!senti</li>
+                <li>
+                  !urban
+                  <span className="tooltip">Description for !urban</span>
+                </li>
+                <li>
+                  !qrcode
+                  <span className="tooltip">Description for !qrcode</span>
+                </li>
+                <li>
+                  !imgur
+                  <span className="tooltip">Description for !imgur</span>
+                </li>
+                <li>
+                  !wiki
+                  <span className="tooltip">Description for !wiki</span>  
+                </li>
+                <li>
+                  !senti
+                  <span className="tooltip">Description for !senti</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 2 (HelpEmbed2)</h2>
             <ul>
-                <li>!mp3</li>
-                <li>!mp4</li>
-                <li>!ascii</li>
-                <li>!asciicm</li>
-                <li>!gpt</li>
+                <li>
+                  !mp3
+                  <span className="tooltip">Description for !mp3</span>
+                </li>
+                <li>
+                  !mp4
+                  <span className="tooltip">Description for !mp4</span>
+                </li>
+                <li>
+                  !ascii
+                  <span className="tooltip">Description for !ascii</span>
+                </li>
+                <li>
+                  !asciicm
+                  <span className="tooltip">Description for !asciicm</span>
+                </li>
+                <li>
+                  !gpt
+                  <span className="tooltip">Description for !gpt</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 3 (HelpEmbed3)</h2>
             <ul>
-                <li>!tts</li>
-                <li>!reminder</li>
-                <li>!nick</li>
-                <li>!whois</li>
-                <li>!resize</li>
+                <li>
+                  !tts
+                  <span className="tooltip">Description for !tts</span>
+                </li>
+                <li>
+                  !reminder
+                  <span className="tooltip">Description for !reminder</span>
+                </li>
+                <li>
+                  !nick
+                  <span className="tooltip">Description for !nick</span>
+                </li>
+                <li>
+                  !whois
+                  <span className="tooltip">Description for !whois</span>
+                </li>
+                <li>
+                  !resize
+                  <span className="tooltip">Description for !resize</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 4 (HelpEmbed4)</h2>
             <ul>
-                <li>!blur</li>
-                <li>!sharpen</li>
-                <li>!sepia</li>
-                <li>!watercolor</li>
-                <li>!grayscale</li>
-                <li>!invert</li>
-                <li>!solarize</li>
+                <li>
+                  !blur
+                  <span className="tooltip">Description for !blur</span>
+                </li>
+                <li>
+                  !sharpen
+                  <span className="tooltip">Description for !sharpen</span>
+                </li>
+                <li>
+                  !sepia
+                  <span className="tooltip">Description for !sepia</span>
+                </li>
+                <li>
+                  !watercolor
+                  <span className="tooltip">Description for !watercolor</span>
+                </li>
+                <li>
+                  !grayscale
+                  <span className="tooltip">Description for !grayscale</span>
+                </li>
+                <li>
+                  !invert
+                  <span className="tooltip">Description for !invert</span>
+                </li>
+                <li>
+                  !solarize
+                  <span className="tooltip">Description for !solarize</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 5 (HelpEmbed5)</h2>
             <ul>
-                <li>!anime</li>
-                <li>!manga</li>
-                <li>!steam</li>
-                <li>!translate</li>
-                <li>!languagecode</li>
+                <li>
+                  !anime
+                  <span className="tooltip">Description for !anime</span>
+                </li>
+                <li>
+                  !manga
+                  <span className="tooltip">Description for !manga</span>
+                </li>
+                <li>
+                  !steam
+                  <span className="tooltip">Description for !steam</span>
+                </li>
+                <li>
+                  !translate
+                  <span className="tooltip">Description for !translate</span>
+                </li>
+                <li>
+                  !languagecode
+                  <span className="tooltip">Description for !languagecode</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Command List 6 (HelpEmbed6)</h2>
             <ul>
-                <li>!texttopdf</li>
-                <li>!wordtopdf</li>
-                <li>!compress</li>
+                <li>
+                  !texttopdf
+                  <span className="tooltip">Description for !texttopdf</span>
+                </li>
+                <li>
+                  !wordtopdf
+                  <span className="tooltip">Description for !wordtopdf</span>
+                </li>
+                <li>
+                  !compress
+                  <span className="tooltip">Description for !compress</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Side Commands/Files</h2>
             <ul>
-                <li>!owner</li>
-                <li>!botpic</li>
-                <li>!repo</li>
+                <li>
+                  !owner
+                  <span className="tooltip">Description for !owner</span>
+                </li>
+                <li>
+                  !botpic
+                  <span className="tooltip">Description for !botpic</span>
+                </li>
+                <li>
+                  !repo
+                  <span className="tooltip">Description for !repo</span>
+                </li>
             </ul>
           </div>
           <div className="subcommands">
             <h2>Admin Commands/Files</h2>
             <ul>
-                <li>!admin</li>
-                <li>!delete</li>
-                <li>!kick</li>
-                <li>!reload</li>
+                <li>
+                  !admin
+                  <span className="tooltip">Description for !admin</span>
+                </li>
+                <li>
+                  !delete
+                  <span className="tooltip">Description for !delete</span>
+                </li>
+                <li>
+                  !kick
+                  <span className="tooltip">Description for !kick</span>
+                </li>
+                <li>
+                  !reload
+                  <span className="tooltip">Description for !reload</span>
+                </li>
             </ul>
           </div>
         </div>
       </div>
       <footer className="footer">
-        <p>UselessBot is not affiliated with Discord or any of its partners.</p>
-        <p>UselessBot is open-source and licensed under the MIT License.</p>
-        <a href="https://github.com/DenizIsikli" target="_blank" rel="noopener noreferrer">Developer</a>
+        <p>UselessBot is not affiliated with Discord or any of its partners. UselessBot is open-source and licensed under the MIT License.</p>
+        <div className="footer-links">
+          <a href="https://github.com/DenizIsikli/UselessBotWebsite" target="_blank" rel="noopener noreferrer">Repository<FaGithub /></a>
+          <a href="https://github.com/DenizIsikli" target="_blank" rel="noopener noreferrer">Developer<FaGithub /></a>
+        </div>
       </footer>
     </div>
   );
